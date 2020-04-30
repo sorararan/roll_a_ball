a2beb1c603aab008d692f616a8652af93a6cdc38
solalalan <masolainota@gmail.com>
2020-04-30 17:58:06 +0900

    敵を増やした、敵がめちゃくちゃに動く

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
     public float speed;
-    [SerializeField] private int id;
     private Rigidbody rb;
+    public float span;
+    private float currentTime;
+    private float currentMoveHorizontal;
+    private float currentMoveVertical;
+
+    void Start() {
+        currentTime = span;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate () {
+        currentTime += Time.deltaTime;
+        if(currentTime > span){
+            currentMoveHorizontal = Random.Range(-1.0f, 1.0f);
+            currentMoveVertical = Random.Range(-1.0f, 1.0f);
+            currentTime = 0f;
+        }
+
+        var movement = new Vector3(currentMoveHorizontal, 0, currentMoveVertical);
+        rb.AddForce(movement * speed);
+    }
 
     void OnCollisionEnter(Collision col) {
         if (col.gameObject.name == "Player") {
